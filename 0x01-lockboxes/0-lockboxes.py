59435367a270cbea5253344256218258bd236d9e
Daniel Obembe <danielobembe1@gmail.com>
2023-01-07 03:53:54 +0100

    lockboxes fixed again

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,24 @@
+#!/usr/bin/python3
+
+"""Exercise to practice interview"""
+
+
 def canUnlockAll(boxes):
-    # Set to store the keys we have seen so far
+    """Set to store the keys we have seen so far"""
     seen = set()
-    # Add the keys from the first box to the set
+    """Add the keys from the first box to the set"""
     seen.update(boxes[0])
-    # Set of boxes we need to check
+    """Set of boxes we need to check"""
     to_check = seen.copy()
 
-    # Keep checking boxes while there are boxes to check
+    """Keep checking boxes while there are boxes to check"""
     while to_check:
-        # Take a box to check
+        """Take a box to check"""
         box = to_check.pop()
-        # Add its keys to the set of seen keys
+        """Add its keys to the set of seen keys"""
         seen.update(boxes[box])
-        # Add the boxes that can be unlocked with the new keys to the set of boxes to check
+        """Add the boxes that can be unlocked with the new keys to the set of boxes to check"""
         to_check.update(set(boxes[box]) - seen)
 
-    # Return whether all boxes have been seen
+    """Return whether all boxes have been seen"""
     return len(seen) == len(boxes)
