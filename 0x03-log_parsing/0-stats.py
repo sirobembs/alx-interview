c16beb203ea8fa36f4cb5736ed688ba1f6f29322
Daniel Obembe <danielobembe1@gmail.com>
2023-01-21 03:28:04 +0100

    finally got it to work

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,41 @@
+#!/usr/bin/python3
+"""Input stats"""
 import sys
 
-# Initialize variables to store metrics
-total_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+stats = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+sizes = [0]
+
+
+def print_stats():
+    print('File size: {}'.format(sum(sizes)))
+    for s_code, count in sorted(stats.items()):
+        if count:
+            print('{}: {}'.format(s_code, count))
+
 
 try:
-    for line in sys.stdin:
-        # Parse the line to extract relevant information
-        parts = line.split(" ")
+    for i, line in enumerate(sys.stdin, start=1):
+        matches = line.rstrip().split()
         try:
-            # Check if the line matches the expected format
-            if parts[0].startswith("<") and parts[2].startswith("[") and parts[4].startswith("\"GET"):
-                status_code = int(parts[8])
-                file_size = int(parts[9])
-                # Update metrics
-                total_size += file_size
-                status_code_counts[status_code] += 1
-                line_count += 1
-        except (ValueError, KeyError):
-            # Skip lines that do not match the expected format or contain invalid status codes
+            status_code = matches[-2]
+            file_size = matches[-1]
+            if status_code in stats.keys():
+                stats[status_code] += 1
+            sizes.append(int(file_size))
+        except Exception:
             pass
-
-        if line_count % 10 == 0:
-            # Print metrics every 10 lines
-            print("Total file size: ", total_size)
-            for status_code in sorted(status_code_counts.keys()):
-                if status_code_counts[status_code] > 0:
-                    print(f"{status_code}: {status_code_counts[status_code]}")
+        if i % 10 == 0:
+            print_stats()
+    print_stats()
 except KeyboardInterrupt:
-    # Print metrics upon keyboard interruption (CTRL + C)
-    print("Total file size: ", total_size)
-    for status_code in sorted(status_code_counts.keys()):
-        if status_code_counts[status_code] > 0:
-            print(f"{status_code}: {status_code_counts[status_code]}")
+    print_stats()
+    raise
