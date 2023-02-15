48e42962d86afbbbc6cb9cfa11c4762cbbe47fbc
Daniel Obembe <danielobembe1@gmail.com>
2023-02-15 08:34:26 +0100

    Star Wars Characters mandatory

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+const https = require('https');
+const process = require('process');
+
+const movieId = process.argv[2];
+
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+https.get(url, (res) => {
+  let rawData = '';
+  res.on('data', (chunk) => { rawData += chunk; });
+  res.on('end', () => {
+    try {
+      const movie = JSON.parse(rawData);
+      const characterUrls = movie.characters;
+      characterUrls.forEach((url) => {
+        https.get(url, (res) => {
+          let rawData = '';
+          res.on('data', (chunk) => { rawData += chunk; });
+          res.on('end', () => {
+            try {
+              const character = JSON.parse(rawData);
+              console.log(character.name);
+            } catch (error) {
+              console.error(error.message);
+            }
+          });
+        }).on('error', (error) => {
+          console.error(error.message);
+        });
+      });
+    } catch (error) {
+      console.error(error.message);
+    }
+  });
+}).on('error', (error) => {
+  console.error(error.message);
+});
